e68486e823a85bcfaae3edcd904428ac3d7824d8
al0cam <bucomacola@gmail.com>
2020-10-08 11:45:37 +0200

    Update substring.rb

--- substring.rb
+++ substring.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+def add_to_hash(output_hash, check_word, word)
+  output_hash[word] = check_word.count(word)
+end
 
 def substring(check_word, substrings)
-  substrings.each_with_index { |word, index| puts word index if check_word.include?(word) }
+  output_hash = Hash[]
+  substrings.map do |word|
+    add_to_hash(output_hash,check_word, word) if check_word.include?(word)
+  end
+  puts output_hash
 end
 
 substring('below', dictionary)
