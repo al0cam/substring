1282c7304da6060f225c3283722bf4c94078f278
Al0cam <bucomacola@gmail.com>
2020-10-10 21:12:25 +0200

    Final changes

--- substring.rb
+++ substring.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-def add_to_hash(output_hash, check_word, word)
-  output_hash[word] = check_word.count(word)
-end
-
 def substring(check_word, substrings)
-  output_hash = Hash[]
+  output_hash = Hash.new(0)
   substrings.map do |word|
-    add_to_hash(output_hash,check_word, word) if check_word.include?(word)
+    output_hash[word] += 1 if check_word.include?(word)
   end
   puts output_hash
 end
